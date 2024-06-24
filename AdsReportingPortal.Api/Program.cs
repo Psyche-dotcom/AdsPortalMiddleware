5102bd0efbdd493804aea7a66002fc0c7211ed87
Saheed Olawale Babatunde <jaydthony@gmail.com>
2024-06-24 08:06:33 +0100

    fix time

--- AdsReportingPortal.Api/Program.cs
+++ AdsReportingPortal.Api/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddQuartz(q =>
         .ForJob(jobKey)
         .WithIdentity("MyJob-trigger")
         .WithSimpleSchedule(x => x
-            .WithInterval(TimeSpan.FromHours(1))
+            .WithInterval(TimeSpan.FromMinutes(12))
             .RepeatForever()));
 });
 
